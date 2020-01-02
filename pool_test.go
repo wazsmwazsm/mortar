6b48a7d916b53d5530a32c6c2edb24f33642c7ed
wazsmwazsm <942443360@qq.com>
2020-01-02 20:52:40 +0800

    update test

--- pool_test.go
+++ pool_test.go
@@ -1,7 +1,7 @@
 package mortar
 
 import (
-	"context"
+	"sync"
 	"sync/atomic"
 	"testing"
 )
@@ -9,7 +9,16 @@ import (
 var sum int64
 var runTimes = 1000000
 
+var wg = sync.WaitGroup{}
+
 func demoTask(v ...interface{}) {
+	for i := 0; i < 100; i++ {
+		atomic.AddInt64(&sum, 1)
+	}
+}
+
+func demoTask2(v ...interface{}) {
+	defer wg.Done()
 	for i := 0; i < 100; i++ {
 		atomic.AddInt64(&sum, 1)
 	}
@@ -27,14 +36,39 @@ func BenchmarkPut(b *testing.B) {
 		b.Error(err)
 	}
 
-	ctx := context.Background()
 	task := &Task{
 		Handler: demoTask,
 	}
 
 	for i := 0; i < b.N; i++ {
-		pool.Put(ctx, task)
+		pool.Put(task)
 	}
+}
+
+func BenchmarkGoroutineTimelife(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		wg.Add(1)
+		go demoTask2()
+	}
+	wg.Wait()
+}
+
+func BenchmarkPutTimelife(b *testing.B) {
+	pool, err := NewPool(10)
+	if err != nil {
+		b.Error(err)
+	}
+
+	task := &Task{
+		Handler: demoTask2,
+	}
+
+	for i := 0; i < b.N; i++ {
+		wg.Add(1)
+		pool.Put(task)
+	}
+	wg.Wait()
+
 }
 
 func BenchmarkGoroutineSetTimes(b *testing.B) {
@@ -44,18 +78,44 @@ func BenchmarkGoroutineSetTimes(b *testing.B) {
 	}
 }
 
-func BenchmarkPutSetTimes(b *testing.B) {
+func BenchmarkPoolPutSetTimes(b *testing.B) {
 	pool, err := NewPool(20)
 	if err != nil {
 		b.Error(err)
 	}
 
-	ctx := context.Background()
 	task := &Task{
 		Handler: demoTask,
 	}
 
 	for i := 0; i < runTimes; i++ {
-		pool.Put(ctx, task)
+		pool.Put(task)
 	}
+}
+
+func BenchmarkGoroutineTimeLifeSetTimes(b *testing.B) {
+
+	for i := 0; i < runTimes; i++ {
+		wg.Add(1)
+		go demoTask2()
+	}
+	wg.Wait()
+}
+
+func BenchmarkPoolTimeLifeSetTimes(b *testing.B) {
+	pool, err := NewPool(20)
+	if err != nil {
+		b.Error(err)
+	}
+
+	task := &Task{
+		Handler: demoTask2,
+	}
+
+	for i := 0; i < runTimes; i++ {
+		wg.Add(1)
+		pool.Put(task)
+	}
+
+	wg.Wait()
 }
